8f4408315a06b8475a62ebd781bc92a2baaa677e
AlessandroCiaccio <146941441+AlessandroCiaccio@users.noreply.github.com>
2023-10-19 16:53:26 +0200

    Added some features

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,11 @@ public class Main {
         Studente studente1=new Studente("Alessandro","Scrofani", 1);
 
         studente1.setNome("Francesco");
-        studente1.setCognome("Viscuso");
+        studente1.setCognome("Motta");
         studente1.setIdentificativo(4);
 
-        System.out.println(studente1.getNome());
-        System.out.println(studente1.getCognome());
-        System.out.println(studente1.getIdentificativo());
-
-        System.out.println(studente1);
+        System.out.println("Nome: " + studente1.getNome());
+        System.out.println("Cognome: " + studente1.getCognome());
+        System.out.println("ID: " + studente1.getIdentificativo());
     }
 }
